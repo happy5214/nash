07ee240abfa8724521a9419114cdc1745e2f6d73
Alexander Jones <happy5214@gmail.com>
2021-10-28 03:14:20 -0500

    Split common code into new module and add Makefile

--- mnash.c
+++ mnash.c
@@ -1,120 +1,21 @@
-/* nash.c
-   version 0.5
-   by Thomas Ritschel
+/* mnash.c
+   version 0.6
+   by Thomas Ritschel and Alexander Jones
    based on Jack Brennen's Java applet
 */
 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <math.h>
 #include <gmp.h>
 
-mpz_t ptab[512];
-mpz_t z;
-int evalue[500];
-int dvalue[500];
-int sva[10000];
-unsigned int ecnt;
-
-void init_gmp(unsigned int b)
-{
-  mp_size_t bits = (mp_size_t) 1024.0*log(b)/log(2.0);  /* this is just a crude estimate */
-  mpz_init(z);
-  mpz_array_init(*ptab, 512, bits);     /* dummy init */
-}
-
-void init_weight(unsigned int b, mpz_t k)
-{
-  unsigned int d, i, n;
-
-  mpz_set(z, k);
-
-  for (n=0; n<=511; n++)
-  {
-/*    mpz_mul_ui(ptab[n], ptab[n-1], 2); */
-    mpz_add_ui(ptab[n], z, 1);
-    mpz_mul_ui(z, z, b);
-/*    gmp_printf("%d*%d^%d-1 = %Zd\n", k, b, n, ptab[n]); */
-  }
-
-  ecnt = 0;
-  for (d=1; d<=256; d++)
-    for (i=0; i<d; i++)
-    {
-      for (n=0; n<ecnt; n++)
-        if (d%dvalue[n] == 0 && i%dvalue[n] == evalue[n])
-	  break;
-      if (n >= ecnt)
-      {
-        mpz_gcd(z, ptab[i], ptab[i+d]);
-        if (mpz_cmp_ui(z, 1) > 0)
-        {
-          evalue[ecnt] = i;
-          dvalue[ecnt] = d;
-          ecnt++;
-       /*   printf("Eliminate %d, step %d\n", i, d); */
-        }
-      }
-    }
-}
-
-int nash()
-{
-  unsigned int i, d, n;
-  for (i=0; i<10000; i++)
-    sva[i] = 1;
-
-  for (n=0; n<ecnt; n++)
-  {
-    i = evalue[n];
-    d = dvalue[n];
-    while (i<110000)
-    {
-      if (i >= 100000)
-        sva[i-100000] = 0;
-      i += d;
-    }
-  }  
-
-  n = 0;
-  for (i=0; i<10000; i++)
-    n += sva[i];
-
-  return(n);
-}
-
-int weight()
-{
-  unsigned int i, d, n;
-  for (i=0; i<10000; i++)
-    sva[i] = 1;
-
-  for (n=0; n<ecnt; n++)
-  {
-    i = evalue[n];
-    d = dvalue[n];
-    while (i<10000)
-    {
-      sva[i] = 0;
-      i += d;
-    }
-  }
-
-  n = 0;
-  for (i=0; i<10000; i++)
-    n += sva[i];
-
-  return(n);
-}
+#include "nash.h"
 
 int main(int argc, char* argv[])
 {
-  unsigned int b;
-  mpz_t k, kstart, kstop, kstep;
-  int n;
-  int w;
+  unsigned int b, n, w;
   int comp;
+  mpz_t k, kstart, kstop, kstep;
   if (argc < 2)
   {
     printf("%s - a tool for computing Nash weights for sequences k*b^n+-1\n\n", argv[0]);
@@ -166,5 +67,5 @@ int main(int argc, char* argv[])
     gmp_printf("%15Zd %d %4d %4d\n", k, b, n, w);
     mpz_add(k, k, kstep);
   }
-  return(0);
+  return 0;
 }
