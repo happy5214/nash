1d4e3b0e46781059a41b2e208e3d36ab1d6ef930
Alexander Jones <happy5214@gmail.com>
2023-11-04 17:49:10 -0500

    Add argument parsing and decouple Riesel status from values

--- cpp/mnash.cpp
+++ cpp/mnash.cpp
@@ -1,65 +1,110 @@
 /* mnash.cpp
-   version 0.7
+   version 0.8
    by Thomas Ritschel and Alexander Jones
    based on Jack Brennen's Java applet
 */
 
 #include <cstdio>
 #include <cstdlib>
+#include <iostream>
+#include <string>
 
 #include <gmpxx.h>
 
+#include "arg_parser.h"
 #include "NashSieve.h"
 
 static const char * invocation_name = "mnash";
 
-int main(int argc, char* argv[]) {
-	unsigned int base;
-	unsigned int standardWeight, prothWeight;
-	int comp;
-	mpz_class k, kstart, kstop, kstep;
+void print_help() {
+	printf("MNash - a tool for computing Nash weights for sequences k*b^n+-1\n\n");
+	printf("usage: %s <kmin> <kmax> <kstep> <b>\n", invocation_name);
+	printf("or:    %s <kmin> <kmax> <kstep>\n\n", invocation_name);
+	printf("If no base <b> is given, b=2 is assumed.\n");
+	printf("By default Proth sequences (k*b^n+1) are assumed.\n");
+	printf("For Riesel sequences (k*b^n-1), add the -r parameter.\n\n\n");
+	printf("Example (computing the Nash weight for k*3^n-1 for k=10 to k=14):\n\n");
+	printf("   %s -r 10 14 2 3\n\n", invocation_name);
+	printf("   10  3 4054 4038\n");
+	printf("   12  3 2359 2369\n");
+	printf("   14  3 1524 1523\n");
+	printf("The first two values are k and b, the third value (1524) is the\n");
+	printf("standard Nash weight for the interval 100000 <= n < 110000.\n");
+	printf("The forth value is the Nash weight for 0 <= n < 10000.\n");
+}
 
+int main(int argc, char* argv[]) {
 	if (argc > 0) {
 		invocation_name = argv[0];
 	}
 
-	if (argc < 2) {
-		printf("%s - a tool for computing Nash weights for sequences k*b^n+-1\n\n", invocation_name);
-		printf("usage: %s <kmin> <kmax> <kstep> <b>\n", invocation_name);
-		printf("or:    %s <kmin> <kmax> <kstep>\n\n", invocation_name);
-		printf("If no base <b> is given, b=2 is assumed.\n");
-		printf("By default Proth sequences (k*b^n+1) are assumed.\n");
-		printf("For Riesel sequences (k*b^n-1) enter k as -k.\n\n\n");
-		printf("Example (computing the Nash weight for k*3^n-1 for k=10 to k=14):\n\n");
-		printf("   %s -14 -10 2 3\n\n", invocation_name);
-		printf("   -14  3 1524 1523\n");
-		printf("   -12  3 2359 2369\n");
-		printf("   -10  3 4054 4038\n");
-		printf("The first two values are k and b, the third value (1524) is the\n");
-		printf("standard Nash weight for the interval 100000 <= n < 110000.\n");
-		printf("The forth value is the Nash weight for 0 <= n < 10000.\n");
+	// Parse arguments
+	const Arg_parser::Option options[] = {
+		{ 'w', "minimum-weight", Arg_parser::yes },
+		{ 'W', "maximum-weight", Arg_parser::yes },
+		{ 'r', "riesel",         Arg_parser::no  },
+		{ 'h', "help",           Arg_parser::no  },
+		{   0, 0,                Arg_parser::no  }
+	};
+
+	const Arg_parser parser( argc, argv, options );
+	if (parser.error().size()) {
+		std::cerr << "Argument error: " << parser.error() << std::endl;
 		return 1;
 	}
 
-	if (argc > 4) {
-		base = (unsigned int) atoi(argv[4]);
-	} else {
-		base = 2;
+	unsigned int minimumWeight = 0;
+	unsigned int maximumWeight = 10000;
+	bool isRiesel = false;
+
+	int argind;
+
+	for (argind = 0; argind < parser.arguments(); ++argind ) {
+		const int code = parser.code(argind);
+		if (!code) {
+			break;
+		}
+		switch (code) {
+			case 'w': minimumWeight = (unsigned int) std::stoi(parser.argument(argind)); break;
+			case 'W': maximumWeight = (unsigned int) std::stoi(parser.argument(argind)); break;
+			case 'r': isRiesel = true; break;
+			case 'h': print_help(); return 0;
+			default :
+				std::cerr << "Uncaught option: " << code << std::endl;
+		}
+	} // end process options
+
+	const std::string k_min = parser.argument( argind++ );
+	if (k_min.empty()) {
+		print_help();
+		return 1;
+	}
+	const std::string k_max = parser.argument( argind++ );
+	if (k_max.empty()) {
+		print_help();
+		return 1;
+	}
+	std::string k_step = parser.argument( argind++ );
+	if (k_step.empty()) {
+		k_step = "2";
+	}
+	std::string base_str = parser.argument( argind++ );
+	if (base_str.empty()) {
+		base_str = "2";
 	}
 
-	kstart = argv[1];
-	kstop = argv[2];
+	unsigned int standardWeight, prothWeight;
 
-	if (argc > 3) {
-		kstep = argv[3];
-	} else {
-		kstep = 2;
-	}
+	mpz_class k, kstart, kstop, kstep;
+	kstart = k_min;
+	kstop = k_max;
+	kstep = k_step;
 
 	k = kstart;
-	comp = cmp(k, kstop);
 
-	int stepSign = sgn(kstep);
+	const unsigned int base = (unsigned int) std::stoi(base_str);
+
+	const int stepSign = sgn(kstep);
 	if (stepSign == 0) {
 		fprintf(stderr, "Cannot use step of 0. Exiting.\n");
 		return 1;
@@ -69,11 +114,16 @@ int main(int argc, char* argv[]) {
 		kstep = -kstep;
 	}
 
+	const int comp = cmp(k, kstop);
+
 	while (cmp(k, kstop) * comp >= 0) {  // run until sign of comparison changes
-		NashSieve siever(base, k);
+		NashSieve siever(base, isRiesel ? -k : k);
 		standardWeight = siever.standard_nash_weight();
 		prothWeight = siever.proth_nash_weight();
-		gmp_printf("%15Zd %d %4u %4u\n", k, base, standardWeight, prothWeight);
+		if ((minimumWeight <= maximumWeight && (standardWeight >= minimumWeight && standardWeight <= maximumWeight)) ||
+			(minimumWeight > maximumWeight && (standardWeight >= minimumWeight || standardWeight <= maximumWeight))) {
+			gmp_printf("%15Zd %d %4u %4u\n", k, base, standardWeight, prothWeight);
+		}
 		k += kstep;
 	}
 
--- cpp/nash.cpp
+++ cpp/nash.cpp
@@ -1,55 +1,90 @@
 /* nash.cpp
-   version 0.7
+   version 0.8
    by Thomas Ritschel and Alexander Jones
    based on Jack Brennen's Java applet
 */
 
 #include <cstdio>
 #include <cstdlib>
+#include <iostream>
+#include <string>
 
 #include <gmpxx.h>
 
+#include "arg_parser.h"
 #include "NashSieve.h"
 
 static const char * invocation_name = "nash";
 
-int main(int argc, char* argv[]) {
-	unsigned int base;
-	unsigned int standardWeight, prothWeight;
-	mpz_class k;
+void print_help() {
+	printf("Nash - a tool for computing Nash weights for sequences k*b^n+-1\n\n");
+	printf("usage: %s <k> <b>\n", invocation_name);
+	printf("or:    %s <k>\n\n", invocation_name);
+	printf("If no base <b> is given, b=2 is assumed.\n");
+	printf("By default Proth sequences (k*b^n+1) are assumed.\n");
+	printf("For Riesel sequences (k*b^n-1), add the -r parameter.\n\n\n");
+	printf("Example (computing the Nash weight for 14*17^n-1):\n\n");
+	printf("   %s -r 14 17\n\n", invocation_name);
+	printf("   14 17  803  800\n\n");
+	printf("The first two values are k and b, the third value (803) is the\n");
+	printf("standard Nash weight for the interval 100000 <= n < 110000.\n");
+	printf("The fourth value is the Nash weight for 0 <= n < 10000.\n");
+}
 
+int main(int argc, char* argv[]) {
 	if (argc > 0) {
 		invocation_name = argv[0];
 	}
 
-	if (argc < 2) {
-		printf("%s - a tool for computing Nash weights for sequences k*b^n+-1\n\n", invocation_name);
-		printf("usage: %s <k> <b>\n", invocation_name);
-		printf("or:    %s <k>\n\n", invocation_name);
-		printf("If no base <b> is given, b=2 is assumed.\n");
-		printf("By default Proth sequences (k*b^n+1) are assumed.\n");
-		printf("For Riesel sequences (k*b^n-1) enter k as -k.\n\n\n");
-		printf("Example (computing the Nash weight for 14*17^n-1):\n\n");
-		printf("   %s -14 17\n\n", invocation_name);
-		printf("   -14 17  803  800\n\n");
-		printf("The first two values are k and b, the third value (803) is the\n");
-		printf("standard Nash weight for the interval 100000 <= n < 110000.\n");
-		printf("The fourth value is the Nash weight for 0 <= n < 10000.\n");
+	// Parse arguments
+	const Arg_parser::Option options[] = {
+		{ 'r', "riesel",         Arg_parser::no  },
+		{ 'h', "help",           Arg_parser::no  },
+		{   0, 0,                Arg_parser::no  }
+	};
+
+	const Arg_parser parser( argc, argv, options );
+	if (parser.error().size()) {
+		std::cerr << "Argument error: " << parser.error() << std::endl;
 		return 1;
 	}
 
-	if (argc > 2) {
-		base = (unsigned int) atoi(argv[2]);
-	} else {
-		base = 2;
+	bool isRiesel = false;
+
+	int argind;
+
+	for (argind = 0; argind < parser.arguments(); ++argind ) {
+		const int code = parser.code(argind);
+		if (!code) {
+			break;
+		}
+		switch (code) {
+			case 'r': isRiesel = true; break;
+			case 'h': print_help(); return 0;
+			default :
+				std::cerr << "Uncaught option: " << code << std::endl;
+		}
+	} // end process options
+
+	const std::string k_str = parser.argument( argind++ );
+	if (k_str.empty()) {
+		print_help();
+		return 1;
+	}
+	std::string base_str = parser.argument( argind++ );
+	if (base_str.empty()) {
+		base_str = "2";
 	}
 
-	k = argv[1];
+	const unsigned int base = (unsigned int) std::stoi(base_str);
+	unsigned int standardWeight, prothWeight;
+	mpz_class k;
+	k = k_str;
 
-	NashSieve siever(base, k);
+	NashSieve siever(base, isRiesel ? -k : k);
 	standardWeight = siever.standard_nash_weight();
 	prothWeight = siever.proth_nash_weight();
-	printf("%s %u %4u %4u\n", argv[1], base, standardWeight, prothWeight);
+	printf("%s %u %4u %4u\n", k_str.c_str(), base, standardWeight, prothWeight);
 
 	return 0;
 }
